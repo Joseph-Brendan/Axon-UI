2c2f16254468f4e7adb0cf9cc10179fd3d27adfb
bigbuck2605 <noniboy101@gmail.com>
2025-01-31 17:35:09 +0100

    Dropdown menu comp

--- axon-ui/src/App.jsx
+++ axon-ui/src/App.jsx
@@ -1,14 +1,30 @@
 // import NavBarComp1 from "./Navigation-Components/NavBar/NavBar1-Components/NavBar1.jsx";
 
-import InputField from "./Form-Components/InputField/InputField";
+//import InputField from "./Form-Components/InputField/InputField";
+import DropDown from "./Form-Components/Dropdown/DropDown";
+import FieldLabel from "./Form-Components/InputField/FieldLabel"
 
 function App() {
+
+  const options = [
+    "Placeholder",
+    "Placeholder",
+    "Placeholder",
+    "Placeholder",
+    "Placeholder"
+  ]
+
+  const handleChange = ()=> {
+    console.log("Selected:, options")
+  }
   return (
     <div>
       {/* <NavBarComp1 
           placeholderTxt="Home"
         /> */}
-      <InputField />
+      {/* <InputField /> */}
+      
+      <DropDown placeholder="Input text" label="label" options={options} onChange={handleChange}/>
     </div>
   );
 }
